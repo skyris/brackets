c81061975c3de669c85414a9db42fac4ea43b4ea
Victor Klimov <4klimov@gmail.com>
2021-02-03 16:33:00 +0300

    feat: Function 'check' is implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const openBrackets = bracketsConfig.map(el => el[0]);
+    const bracketsPairMap = bracketsConfig
+        .reduce((acc, arr) => (acc[arr[0]] = arr[1], acc), {});
+    const stack = [];
+    const isClosedForPrevious = el => {
+        return bracketsPairMap[stack[stack.length - 1]] === el;
+    }
+
+    for (const el of str) {
+        if (isClosedForPrevious(el)) {
+            stack.pop();
+        } else if (openBrackets.includes(el)) {
+            stack.push(el);
+        } else {
+            return false;
+        }
+    }
+    
+    if (stack.length) return false;
+    
+    return true;
 }
+
+
